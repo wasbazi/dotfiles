f02b4008400f47661ac7fb6994566af2c4a96e86
Avery Finn <wasbazi@gmail.com>
2019-04-27 20:12:54 +0000

    update various settings

--- scripts/setup/links.sh
+++ scripts/setup/links.sh
@@ -5,10 +5,15 @@ REPO_DIR="$(pwd)"
 VIM_HOME="$(pwd)/vim"
 echo "repo dir: $REPO_DIR"
 
-ln -sf $VIM_HOME ~/.vim
+ln -sf "$VIM_HOME" ~/.vim
 ln -sf ~/.vim/vimrc ~/.vimrc
 mkdir -p ~/.config
 ln -sf ~/.vim ~/.config/nvim
 
-ln -sf $REPO_DIR/zsh/zshrc ~/.zshrc
-ln -sf $REPO_DIR/tmux.conf ~/.tmux.conf
+ln -sf "$REPO_DIR/zsh/zshrc" ~/.zshrc
+
+if [ -z "$ABYSS_SATELLITE" ]; then
+  ln -sf "$REPO_DIR/tmux.conf" ~/.tmux.conf
+else
+  ln -sf "$REPO_DIR/tmux.linux.conf" ~/.tmux.conf
+fi
